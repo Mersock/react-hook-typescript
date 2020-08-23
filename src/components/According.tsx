90489a402365c6bf043cf05d05cb13237996cf50
Knight <knzphumthawan@gmail.com>
2020-08-23 16:59:04 +0700

    type with according

--- src/components/According.tsx
+++ src/components/According.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {
     items: { title: string, content: string }[]
 }
 
 const According = ({ items }: Props) => {
-    return <h1>{items.length}</h1>
+    const [activeIndex, setactiveIndex] = useState<null | number>(null)
+
+    const onTitileClick = (index: number): void => {
+        setactiveIndex(index)
+    }
+
+    const renderedItems: JSX.Element[] = items.map((item: { title: string, content: string }, index: number) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div
+                    className="title active"
+                    onClick={() => onTitileClick(index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p >
+                        {item.content}
+                    </p>
+                </div>
+            </React.Fragment >
+        )
+    })
+
+    return <div className="ui styled accordion">
+        {renderedItems}
+        <h1>{activeIndex}</h1>
+    </div>
 }
 
 export default According
